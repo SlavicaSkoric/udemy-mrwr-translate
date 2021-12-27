014693b080fec596117ff04e2f1a4b2d05ec7b5b
David Filipovic <david@realigned.co>
2021-12-27 10:02:08 +0100

    init and add select a language functionality

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+  //we make sure this is defined as an arrow function because it is a callback method that we are passing to something else in our application
+
+  render() {
+    return (
+      <div className='ui container'>
+        <div>
+          Select a language:
+          <i
+            className='flag us'
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className='flag nl'
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
+  }
+}
+
+export default App;
