97ece02fc1fba1004674e2a6c0e6344b54462a3a
Slavica Skorić <skoric.slavica.hr@gmail.com>
2021-12-27 11:11:39 +0100

    hooking up context values

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+  //we hook up a context object to a class component above
+  //contextType - is a property
+  //static - keyword - adds a contextType property directly to our class (way 1)
+
   render() {
-    return <button className='ui button primary'>Submit</button>;
+    //console.log(this.context);
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return <button className='ui button primary'>{text}</button>;
   }
 }
+
+//Button.contextType = LanguageContext;
+//(way 2)
 
 export default Button;
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,5 @@
+//create a context object and export it to import it and make use of it only where we actually need it - where we want to connect it
+import React from 'react';
+
+export default React.createContext('english');
+//default value passed directly into the context object
